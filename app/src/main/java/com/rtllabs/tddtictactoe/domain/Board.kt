be24d64421118e8ecf266afbd4d01c2c38a7f0ba
rtljava2018 <rtljava2016@gmail.com>
2026-02-07 18:00:17 +0530

    Tested game new board should have empty cells

---
+++ app/src/main/java/com/rtllabs/tddtictactoe/domain/Board.kt
@@ -0,0 +1,7 @@
+package com.rtllabs.tddtictactoe.domain
+
+class Board {
+    fun isEmpty(): Boolean {
+        return true
+    }
+}
---
+++ app/src/test/java/com/rtllabs/tddtictactoe/domain/BoardTest.kt
@@ -0,0 +1,15 @@
+package com.rtllabs.tddtictactoe.domain
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class BoardTest {
+
+    @Test
+    fun gameNewBoardShouldHaveEmptyCells() {
+        val board = Board()
+
+        assertTrue(board.isEmpty())
+
+    }
+}
