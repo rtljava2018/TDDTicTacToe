9613d9863785086d30449aa763b2c5bcad7e1f95
rtljava2018 <rtljava2016@gmail.com>
2026-02-08 14:29:04 +0530

    Refactor Tested game should start with player X

--- app/src/main/java/com/rtllabs/tddtictactoe/domain/TicTacToeGame.kt
+++ app/src/main/java/com/rtllabs/tddtictactoe/domain/TicTacToeGame.kt
@@ -2,13 +2,16 @@ package com.rtllabs.tddtictactoe.domain
 
 class TicTacToeGame {
     private val board = Board()
+    private var currentPlayer = Player.X
+
+
 
     fun getBoard(): Board {
         return board
     }
 
     fun getCurrentPlayer(): Player {
-        return Player.X
+        return currentPlayer
     }
 
 }
