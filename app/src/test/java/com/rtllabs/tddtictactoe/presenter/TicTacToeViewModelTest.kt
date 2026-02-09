d4ec66e173efb451511b24003eaafae53a6e56d2
rtljava2018 <rtljava2016@gmail.com>
2026-02-09 22:17:49 +0530

    Tested TicTacToc viewModel InitialState should be InProgress and board cells size and initial player

--- app/src/test/java/com/rtllabs/tddtictactoe/presenter/TicTacToeViewModelTest.kt
+++ app/src/test/java/com/rtllabs/tddtictactoe/presenter/TicTacToeViewModelTest.kt
@@ -1,9 +1,11 @@
 package com.rtllabs.tddtictactoe.presenter
 
 import app.cash.turbine.test
+import com.rtllabs.tddtictactoe.domain.entity.Player
 import com.rtllabs.tddtictactoe.presentation.GameUiState
 import com.rtllabs.tddtictactoe.presentation.TicTacToeViewModel
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TicTacToeViewModelTest {
@@ -17,6 +19,9 @@ class TicTacToeViewModelTest {
             val initialState = awaitItem()
 
             assert(initialState is GameUiState.GameInProgress)
+            val inProgress=initialState as GameUiState.GameInProgress
+            assert(inProgress.board.flatten().size==9)
+            assertEquals(Player.X,inProgress.currentPlayer)
             cancelAndIgnoreRemainingEvents()
         }
     }
