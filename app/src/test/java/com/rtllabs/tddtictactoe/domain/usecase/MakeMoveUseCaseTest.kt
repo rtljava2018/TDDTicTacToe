8f8dcb33e0abb83551560f6515b0f9d4f9503095
rtljava2018 <rtljava2016@gmail.com>
2026-02-09 11:12:53 +0530

    Tested makeMoveUseCase should detect column win return state currentPlayer winner isDraw isGameOver

--- app/src/test/java/com/rtllabs/tddtictactoe/domain/usecase/MakeMoveUseCaseTest.kt
+++ app/src/test/java/com/rtllabs/tddtictactoe/domain/usecase/MakeMoveUseCaseTest.kt
@@ -59,4 +59,19 @@ class MakeMoveUseCaseTest {
         assertTrue(gameState.isGameOver)
     }
 
+    @Test
+    fun makeMoveUseCaseShouldDetectColumnWin(){
+
+        useCase(0,0)//X
+        useCase(1,1)//O
+        useCase(1,0)//X
+        useCase(2,1)//O
+        val gameState=useCase(2,0)//X
+
+        assertEquals(Player.X, gameState.board[0][0])
+        assertNotNull(gameState.winner)
+        assertFalse(gameState.isDraw)
+        assertTrue(gameState.isGameOver)
+    }
+
 }
