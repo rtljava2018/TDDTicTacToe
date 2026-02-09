4edffeaa2dde273524c29a56b7c72b762fb47744
rtljava2018 <rtljava2016@gmail.com>
2026-02-09 23:38:27 +0530

    Tested TicTacToc viewModel onCellClicked should be Return Draw state

--- app/src/test/java/com/rtllabs/tddtictactoe/presenter/TicTacToeViewModelTest.kt
+++ app/src/test/java/com/rtllabs/tddtictactoe/presenter/TicTacToeViewModelTest.kt
@@ -10,7 +10,6 @@ import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
